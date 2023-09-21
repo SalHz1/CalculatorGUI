ac19f0e6a5ad1c1929a086be8d08954e3c6bb72c
SalHz1 <sal.hernandez1030@gmail.com>
2023-09-21 17:42:41 -0500

    Defined digit_pressed() function to print out a debug statement TEST to ensure the connect statements in MainWindow constructor are working properly

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,16 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     setWindowTitle("");// This sets an empty application title, otherwise it will
     //display "MainWindow"
-
+    connect(ui->pushButton_num0,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num1,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num2,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num3,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num4,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num5,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num6,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num7,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num8,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
+    connect(ui->pushButton_num9,SIGNAL(released() ),this,SLOT(digit_pressed() ) );
 
 }
 
@@ -16,4 +25,7 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
+void MainWindow::digit_pressed()
+{
+    qDebug() << "Test";
+}
